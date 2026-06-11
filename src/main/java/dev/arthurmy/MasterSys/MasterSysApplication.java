3c494c97115cba9c9e30343d5e2a99192e535af4
ArthurMitsuoYamamoto <arthuryamamoto0@gmail.com>
2026-06-11 11:08:01 -0300

    feat: Projeto criado e properties configurado para o banco de dados

---
+++ src/main/java/dev/arthurmy/MasterSys/MasterSysApplication.java
@@ -0,0 +1,13 @@
+package dev.arthurmy.MasterSys;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MasterSysApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MasterSysApplication.class, args);
+	}
+
+}
