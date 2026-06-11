8e057d2ac2ef3abd0ad7878215cf66531a0e3037
ArthurMitsuoYamamoto <arthuryamamoto0@gmail.com>
2026-06-11 12:12:23 -0300

    feat: conexao com bd criada e projeto inicial criado

--- src/main/java/dev/arthurmy/MasterSys/MasterSysApplication.java
+++ src/main/java/dev/arthurmy/MasterSys/MasterSysApplication.java
@@ -1,4 +1,4 @@
-package dev.arthurmy.MasterSys;
+package dev.arthurmy.mastersys;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
