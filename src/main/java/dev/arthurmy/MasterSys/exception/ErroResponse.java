186a95a1454bf8aff82ffbc72135cc087977fc89
ArthurMitsuoYamamoto <arthuryamamoto0@gmail.com>
2026-06-13 13:15:44 -0300

    feat: melhorando camada de tratamento de erros

---
+++ src/main/java/dev/arthurmy/MasterSys/exception/ErroResponse.java
@@ -0,0 +1,14 @@
+package dev.arthurmy.mastersys.exception;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+public record ErroResponse(
+    LocalDateTime timestamp,
+    Integer status,
+    String erro,
+    List<String> mensagens
+
+) {
+    
+}
